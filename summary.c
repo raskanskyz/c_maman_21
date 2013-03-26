f83fc764abf1d98fb231c7231a76fc95d7c7eb63
raskanskyz <raskanskyz@gmail.com>
2013-03-26 18:08:22 +0200

    n/a

--- summary.c
+++ summary.c
@@ -1,1 +1,15 @@
-test
+#include <stdio.h>
+#include <stdlib.h>
+
+/*function prototype*/
+void summary(int a[], int length);
+
+int main()
+{
+	return 0;
+}/*main*/
+
+void summary(int a[], int length)
+{
+	
+}/*summary*/
