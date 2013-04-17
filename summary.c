4745825d7fa7e507cf52acb75d7bd2fbd25d230b
Eran Raskansky <raskanskyz@gmail.com>
2013-04-17 20:20:11 +0300

    fixed 'printArray' function

--- summary.c
+++ summary.c
@@ -2,56 +2,61 @@
 #include <stdlib.h>
 #define MAX 50
 
-   /*function prototype*/
- int* summary(int a[], int length);
- void printArray(int array[], int i);
+/*function prototype*/
+void summary(int a[], int length);
+void printArray(int array[], int i);
 
-int main()
-{
-    int k;
-    int length=0;
-    int userArray[MAX];
-    printf("Enter some numbers:\n");
-    while(scanf("%d", &k) == 1){
-        userArray[length] = k;
-        length++;
-    }/*while*/
-    
-    summary(userArray, length);
-    printf("\n");
-    
-    return 0;
+int main(){
+	int k;
+	int length=0;
+	int userArray[MAX];
+	printf("Enter some numbers:\n");
+	while((scanf("%d", &k) ==1)){
+		userArray[length] = k;
+		length++;
+
+		if(getchar() == '\n'){
+			summary(userArray, length);
+			length = 0;
+			printf("\n");
+		}	
+	}/*while*/
+
+	summary(userArray, length);
+	printf("\n");
+
+	return 0;
 }/*main*/
 
-int* summary(int a[], int length){
-    int i;
-    int counter = 0;
-    int *p;
-    p = (int *) malloc(length * sizeof(int));
-    
-    if(!p){
-        printf("memory allocation failed!");
-        exit(0);
-    }/*if null*/
-    
-    
-    for(i=0 ; i<length ; i++){
-        
-        counter+=a[i];
-        p[i] = counter;
-   
-    }/*for*/
-    printArray(p, 0);
-    return p;
-    free(p);
+void summary(int a[], int length){
+	int i;
+	int counter = 0;
+	int *p;
+	p = (int *) malloc(length * sizeof(int) + 1);
+
+	if(!p){
+		printf("memory allocation failed!");
+		exit(0);
+	}/*if null*/
+
+
+	for(i=0 ; i<length ; i++){
+
+		counter+=a[i];
+		p[i] = counter;
+
+	}/*for*/
+	p[length] = '\0';
+	printArray(p, 0);
+	free(p);
 }/*summary*/
 
 void printArray(int array[], int i){
-    if(array[i]=='\0'){
-	 return;
+	if(array[i]=='\0'){
+		return;
 	}
-    else{
-        printf("%d ", array[i]);
-        printArray(array, i+1);
-    }
+	else{
+		printf("%d ", array[i]);
+		printArray(array, i+1);
+	}
 }/*prinArray*/
---
+++ test/test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by RaskanskyzMac on 3/27/13.
+//  Copyright (c) 2013 RaskanskyzMac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
